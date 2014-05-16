870ab5fae83f2a0f432dd9bab720902651e09dff
Mike Fikes <mike@fikesfarm.com>
2014-05-15 23:42:09 -0400

    Initial cut at a "solution"

--- vienna.clj
+++ vienna.clj
@@ -18,7 +18,8 @@
    :nodes    {#{:a :b} :y
               #{:a :e} :g
               #{:b :f} :g
-              #{:f :e} :g}})
+              #{:f :e} :g}
+   :home     :f})
 
 (defn map-vals
   [m f]
@@ -32,3 +33,59 @@
       (update-in
         [:nodes]
         (fn [nodes] (map-vals nodes next-light)))))
+
+(defn adjacent-streets
+  "Returns the set of streets adjacent to a given location in the world."
+  [world location]
+  (filter location (keys (:nodes world))))
+
+(defn not-red?
+  "Returns true iff a given street does not currently have a red light."
+  [world street]
+  (not= :r (get-in world [:nodes street])))
+
+(defn candidate-streets
+  "Returns the set of streets that it is legal ride onto given the current location.
+  (These are the adjacent streets without red lights that don't return to previous location.)"
+  [world]
+  (let [non-red-streets (filter (partial not-red? world) (adjacent-streets world (:location world)))]
+    (if (:last-location world)
+      (filter #(not (contains? % (:last-location world))) non-red-streets)
+      non-red-streets)))
+
+(defn take-street
+  "Takes a street, moving the rider to a new location via that street. Returns the new
+  world state."
+  [world street]
+  (let [new-location (first (filter #(not= % (:location world)) street))]
+    (move-to world new-location)))
+
+(defn homeward?
+  [world street]
+  (when (contains? street (:home world))
+    street))
+
+(defn choose-street
+  "Chooses a street to take."
+  [world streets]
+  (if-let [homeward (some (partial homeward? world) streets)]
+    homeward
+    (first streets)))                                       ; We simply choose the first street
+
+(defn not-home?
+  "Returns true iff the rider is in the home location in the world."
+  [world]
+  (not= (:location world) (:home world)))
+
+(defn step-forward
+  "Takes the world forward one iteration step. The set of candidate streets are identified,
+  one of those streets is chosen, and then taken. After the home location is reached, nil
+  will be returned."
+  [world]
+  (when (not-home? world)
+    (take-street world (choose-street world (candidate-streets world)))))
+
+(defn solution
+  "Takes the inital state of the world and iterates to the solution."
+  [world]
+  (take-while identity (iterate step-forward world)))
